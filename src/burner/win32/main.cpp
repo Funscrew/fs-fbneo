b2585572ce170bc09cbe2716e84be450566951ab
Andrew Ritz <aritz@rugsdirect.com>
2025-08-07 00:02:59 -0400

    Revert "- Fixed a bug where the input mapping feature was not available." --> FBN driver system is brittle trash, and I don't feel like hunting down the typo that is breaking the build, so I'll just do a revert instead.
    This reverts commit 96d87a84012d265ec0d92c220342a892227268fb.

--- src/burner/win32/main.cpp
+++ src/burner/win32/main.cpp
@@ -393,7 +393,7 @@ void CloseDebugLog()
 int OpenDebugLog()
 {
 #if defined (FBNEO_DEBUG)
-#if defined (APP_DEBUG_LOG)
+  #if defined (APP_DEBUG_LOG)
 
   time_t nTime;
   tm* tmTime;
@@ -1104,9 +1104,7 @@ int ProcessCommandLine(LPSTR lpCmdLine)
   {
     directOps.romName = romName;
     int res = HandleDirectConnection(directOps);
-    if (res != 0) {
-      return res;
-    }
+    return res;
   }
   else if (load->parsed())
   {
@@ -1117,14 +1115,14 @@ int ProcessCommandLine(LPSTR lpCmdLine)
       throw std::exception("File does not exist!");
     }
     if (ends_with(loadPath, ".fs")) {
-      if (!BurnStateLoad(ANSIToTCHAR(loadPath.data(), NULL, NULL), 1, &DrvInitCallback)) {
-        return 1;
+      if (BurnStateLoad(ANSIToTCHAR(loadPath.data(), NULL, NULL), 1, &DrvInitCallback)) {
+        return 0;
       }
     }
     else if (ends_with(loadPath, ".fr"))
     {
-      if (!StartReplay(ANSIToTCHAR(loadPath.data(), NULL, NULL))) {
-        return 1;
+      if (StartReplay(ANSIToTCHAR(loadPath.data(), NULL, NULL))) {
+        return 0;
       }
     }
     else {
@@ -1160,6 +1158,8 @@ int ProcessCommandLine(LPSTR lpCmdLine)
     }
   }
 
+
+    // NOTE: This is an arbitrary place to pick it back up :)
   POST_INITIALISE_MESSAGE;
 
   if (!nVidFullscreen) {
